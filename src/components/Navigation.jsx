24a0509118162e880eff06f1af02a4cc677c8621
Salman Setiawan <salmansetiawan88@gmail.com>
2025-11-20 10:28:01 +0800

    deactive navbar button

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { textSmall } from '../data/uiStyle'
+import {  } from '../data/uiStyle'
 import LanguageToggle from './LanguageToggle'
 
 const Navigation = () => {
@@ -7,10 +7,10 @@ const Navigation = () => {
     <div className='fixed top-0 z-999 flex justify-center w-full backdrop-blur-xs bg-[#0c0c0c]/50'>
       <div className="flex justify-between w-full items-center max-w-[980px] px-5 py-2">
         <img src="/vite.svg" alt="brand" className="w-7 h-7" />
-        <div className="flex gap-x-1">
+        {/* <div className="flex gap-x-1">
           <button className={`px-2 py-1.5 ${textSmall} font-medium cursor-pointer hover:bg-[#202020] rounded-md`}>Home</button>
           <button className={`px-2 py-1.5 ${textSmall} font-medium cursor-pointer hover:bg-[#202020] rounded-md`}>About Us</button>
-        </div>
+        </div> */}
         <LanguageToggle />
       </div>
     </div>
